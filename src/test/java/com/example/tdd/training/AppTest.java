8167b8ddae2639e1e8b2fd04ce1c0bdf908f8910
kyonmm <kyon.mm@gmail.com>
2019-07-01 11:01:04 +0900

    use junit5 on idea

--- src/test/java/com/example/tdd/training/AppTest.java
+++ src/test/java/com/example/tdd/training/AppTest.java
@@ -3,11 +3,11 @@
  */
 package com.example.tdd.training;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
-    @Test public void testAppHasAGreeting() {
+class AppTest {
+    @Test void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
